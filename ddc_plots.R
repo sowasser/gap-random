92322682f75d381494a436a19bcb47cc7a8595a5
Sophia Wassermann <hi@sowasser.com>
2023-09-22 17:20:19 -0700

    Plotting 2023 data against Lukas's 2022 data

--- ddc_plots.R
+++ ddc_plots.R
@@ -33,31 +33,44 @@ ggsave(specimen_map, filename = here("plots", "specimen_map.png"),
 
 ### Calculate difference between two datasets ---------------------------------
 # Read in DDC code results
-ddc2022_NBS18 <- read.csv(here("DDC comparison", "ddc_2022_NBS18", "VAST_ddc_all_2022.csv"))
+ddc2023 <- read.csv(here("DDC comparison", "ddc_2023", "VAST_ddc_all_2023.csv"))
 ddc2022_Lukas <- read.csv(here("DDC comparison", "ddc_2022_Lukas", "VAST_ddc_all_2022.csv"))
 
-# Plot 2018 NBS data
-nbs18 <- ddc2022_NBS18 %>% filter(year == 2018)
-
+# Plot 2018 data
 ggplot(data = world) +
   geom_sf() +
-  geom_point(data = nbs18,
+  geom_point(data = ddc2023 %>% filter(year == 2018),
              aes(x = start_longitude, y = start_latitude, 
                  size = ddc_cpue_kg_ha, color = ddc_cpue_kg_ha, alpha = ddc_cpue_kg_ha)) +
   coord_sf(xlim = c(-179, -157), ylim = c(54, 66), expand = FALSE) +
   scale_color_viridis(option = "mako", discrete = FALSE, direction = -1, end = 0.9) +
   xlab(" ") + ylab(" ")
 
-ddc2022_NBS18 <- ddc2022_NBS18 %>% filter(year < 2023)
-ddc2022_NBS18$difference <- ddc2022_NBS18$ddc_cpue_kg_ha - ddc2022_Lukas$ddc_cpue_kg_ha
+# Plot 2023 data
+ggplot(data = world) +
+  geom_sf() +
+  geom_point(data = ddc2023 %>% filter(year == 2023),
+             aes(x = start_longitude, y = start_latitude, 
+                 size = ddc_cpue_kg_ha, color = ddc_cpue_kg_ha, alpha = ddc_cpue_kg_ha)) +
+  coord_sf(xlim = c(-179, -157), ylim = c(54, 66), expand = FALSE) +
+  scale_color_viridis(option = "mako", discrete = FALSE, direction = -1, end = 0.9) +
+  xlab(" ") + ylab(" ")
 
+# Plot difference between them 
+ddc_diff <- ddc2023 %>% filter(year < 2023)
+ddc_diff$difference <- ((ddc_diff$ddc_cpue_kg_ha - ddc2022_Lukas$ddc_cpue_kg_ha) / ddc2022_Lukas$ddc_cpue_kg_ha) * 100
+is.na(ddc_diff$difference) <- 0
+
+limit <- max(abs(ddc_diff$difference)) * c(-1, 1)
 ggplot(data = world) +
   geom_sf() +
   geom_point(data = ddc2022_NBS18,
              aes(x = start_longitude, y = start_latitude, 
                  size = abs(difference), color = difference, alpha = abs(difference))) +
   coord_sf(xlim = c(-179, -157), ylim = c(54, 66), expand = FALSE) +
-  scale_color_viridis(option = "mako", discrete = FALSE, direction = -1, end = 0.9) +
-  xlab(" ") + ylab(" ") +
+  scale_color_gradientn(colors = pals::brewer.prgn(100), limit = limit) +
+  xlab(" ") + ylab(" ") + labs(color = "% difference",
+                               size = "% difference",
+                               alpha = "% difference") +
   facet_wrap(~ year)
 
