d907d670bdbfbe697c643de4f25b83c175cb1df9
Sophia Wassermann <hi@sowasser.com>
2024-10-08 14:03:08 -0700

    updated search terms

--- stuff_for_em.R
+++ stuff_for_em.R
@@ -2,61 +2,56 @@ library(rvest)
 
 yrs <- 1950:2024
 
-# searches <- data.frame("type" = "dummy", 
-#                        "search" = "%22as+of+my+last+knowledge+update%22+-%22ChatGPT%22")
-
 searches <- data.frame(type = "Bottom trawls", 
-                       search = '"bottom trawl" OR "beam trawl" OR "otter trawl" OR "trawl" OR "groundfish" OR "shelfish" ') 
+                       search = '"bottom trawl" OR "beam trawl" OR "otter trawl" OR "trawl" OR "groundfish" OR "shelfish" OR ("Fishery independent" OR "Fisheries independent" OR "Fishery-independent" OR "Fisheries-independent" OR "ecosystem survey" OR "Fishery independent data" OR "survey" OR "marine" OR "ocean" )') 
 
 searches <- dplyr::bind_rows(searches, 
                              data.frame(type = "Passive-acoustic", 
-                                        search = '"Acoustic" OR "pelagic" OR "near-bottom" OR "near-surface" OR "mooring" OR "drone" OR "passive acoustics" OR "hydrophone" '))
+                                        search = ' "passive acoustic" OR acoustic OR "mooring" OR "drone"  OR "hydrophone" OR "saildrone" OR "near bottom" OR "near surface" OR marine OR "Fishery independent" OR "Fisheries independent" OR "Fishery independent" OR "Fisheries independent" OR "ecosystem survey" OR "Fishery independent data" OR survey OR marine OR ocean "passive" -acoustic '))
 
 searches <- dplyr::bind_rows(searches, 
                              data.frame(type = "Acoustic trawls", 
-                                        search = '"Acoustic" OR "pelagic" OR "near-bottom" OR "near-surface" OR "active acoustics" OR "pelagic trawl" OR "trawl" OR "Mid water trawl" OR "Mid-water trawl" OR "Midwater trawl"'))
+                                        search = ' "active acoustic" OR acoustic OR pelagic OR "near bottom" OR "near surface" OR marine OR "Fishery independent" OR "Fisheries independent" OR "Fishery independent" OR "Fisheries independent" OR "ecosystem survey" OR "Fishery independent data" OR survey OR marine OR ocean "acoustic" -passive '))
 
 searches <- dplyr::bind_rows(searches, 
                              data.frame(type = "Mid-water trawls", 
-                                        search = '"Mid water trawl" OR "Mid-water trawl" OR "Midwater trawl" OR "pelagic" OR "" OR "Acoustic" OR "pelagic" OR "near-bottom" OR "near-surface" '))
+                                        search = ' ("Mid water trawl" OR "Mid-water trawl" OR "Midwater trawl") AND ("marine" OR "ocean" OR "ecology" OR "biology" OR "fauna" OR "pelagic") OR ("Fishery independent" OR "Fisheries independent" OR "Fishery-independent" OR "Fisheries-independent" OR "ecosystem survey" OR "Fishery independent data" OR "survey" OR "near-bottom" OR "near-surface") ) '))
 
 searches <- dplyr::bind_rows(searches, 
                              data.frame(type = "Dredge", 
-                                        search = '"dredge" OR "benthos" OR "groundfish" OR "shelfish"'))
-
+                                        search = '"dredge" AND "benthos" AND ("marine" OR "ocean" OR "ecology" OR "biology" OR "fauna" OR "groundfish" OR "shelfish" OR "survey") ) '))
 searches <- dplyr::bind_rows(searches, 
                              data.frame(type = "eDNA", 
-                                        search = '"edna" OR "e-dna"'))
+                                        search = '"edna" OR "e-dna" AND ("marine" OR "ocean" OR "ecology" OR "biology" OR "fauna") '))
 
 searches <- dplyr::bind_rows(searches, 
                              data.frame(type = "Stationary gear", 
-                                        search = '"stationary gear" OR "pot" OR "trap" OR "HOOK AND LINE" OR "HOOK & LINE" OR "LONGLINE" OR "groundfish" OR "shelfish" OR "pelagic" '))
+                                        search = ' "stationary gear" OR "pot" OR "trap" OR "HOOK AND LINE" OR "HOOK & LINE" OR "LONGLINE" AND ("marine" OR "ocean" OR "ecology" OR "biology" OR "fauna" OR "survey") ')) 
 
 searches <- dplyr::bind_rows(searches, 
                              data.frame(type = "Visual methods", 
-                                        search = '"camera" OR "scuba" OR "video" OR "HABCAM"'))
+                                        search = ' "camera" OR "scuba" OR "snorkel" OR "video" OR "HABCAM" AND ("marine" OR "ocean" OR "ecology" OR "biology" OR "fauna" OR "survey") '))
 
 searches <- dplyr::bind_rows(searches, 
                              data.frame(type = "Mark-recapture", 
-                                        search = '"mark-recapture" OR "mark recapture" OR "mark recovery" OR "ptag" OR "foy tag" '))
+                                        search = ' "mark-recapture" OR "mark recapture" OR "mark recovery" OR "ptag" OR "foy tag" OR "tagging" OR "tracking AND ("marine" OR "ocean" OR "ecology" OR "biology" OR "fauna") '))
 
 searches <- dplyr::bind_rows(searches, 
                              data.frame(type = "Aerial surveys", 
-                                        search = '"drone" OR "drones" OR "lidar" OR "plane" OR "aerial" OR "SURFACE" OR "Mid-water" OR "midwater" OR "mid water" ' ))
+                                        search = ' "drone" OR "lidar" OR "airplane" OR "helicopter" OR "aerial" OR "imagery" OR "fauna" ("surface" OR "Mid-water" OR "midwater" OR "mid water") AND ("marine" OR "ocean" OR "ecology" OR "biology" OR "fauna" OR "survey") -"pollution" -"oil" -"oil spill" ' ))
 
 
 searches <- dplyr::bind_rows(searches, 
                              data.frame(type = "All FIS", 
-                                        search = '"survey" '))
+                                        search = ' "Fishery independent survey" OR "Fisheries independent survey" OR "Fishery-independent survey" OR "Fisheries-independent survey" OR "fish survey" OR "ecosystem survey" OR "Fishery independent data" OR "survey" OR "monitor" AND ("marine" OR "ocean" OR "ecology" OR "biology" OR "fauna" OR "groundfish" OR "shelfish" OR "pelagic") '))
 
-searches$search <- paste0('"Fishery independent survey" OR "Fisheries independent survey" OR "Fishery-independent survey" OR "Fisheries-independent survey" OR "fish survey" OR "ecosystem survey" OR "Fishery independent data" OR ', searches$search)
 
 searches$search <- gsub(pattern = '"', replacement = "%22", x = searches$search)
 searches$search <- gsub(pattern = ' OR ', replacement = "|", x = searches$search)
 # searches$search <- gsub(pattern = ' AND ', replacement = "+", x = searches$search)
 searches$search <- gsub(pattern = ' ', replacement = "+", x = searches$search)
 
-for (searches0 in 7:length(searches$search)){
+for (searches0 in 8:length(searches$search)){
   numpubs <- c()
   print(searches0)
   for (yr in yrs) {
@@ -75,6 +70,6 @@ for (searches0 in 7:length(searches$search)){
                  "pubs" = as.numeric(gsub(",", "", stringr::str_extract(page[[2]], "\\d{1,3}(,\\d{3})*")))))
     
   }
-  write.csv(x = numpubs, file = here::here("data", paste0("numpubs_", searches0,".csv")))
+  write.csv(x = numpubs, file = here::here(paste0("numpubs_", searches0,".csv")))
   
 }
